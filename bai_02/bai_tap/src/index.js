33922d65fca298a5f4ff13a114ea7b6ff267d16e
tranphuong123321 <phuong23628@gmail.com>
2022-06-08 12:50:08 +0700

    push

--- bai_02/bai_tap/src/index.js
+++ bai_02/bai_tap/src/index.js
@@ -1,11 +1,34 @@
-var n1 = 0;
-var n2 = 1;
-var total;
-var tong = 1;
-for (var i = 1; i <= 20; i++) {
-    total = n1 + n2;
-    n1 = n2;
-    n2 = total;
-    tong += total;
+// let n1: number;
+// n1=0
+// let n2: number;
+// n2=1
+// let total: number;
+// let tong: number;
+// tong=1
+// for (let i = 1; i <= 20; i++) {
+//     total = n1 + n2;
+//     n1 = n2;
+//     n2 = total;
+//     tong += total;
+// }
+// console.log(tong);
+function sumFibo(num) {
+    var n1;
+    n1 = 0;
+    var n2;
+    n2 = 1;
+    var n3;
+    var sum;
+    sum = 1;
+    var list = [];
+    for (var i = 1; i <= num; i++) {
+        n3 = n1 + n2;
+        n1 = n2;
+        n2 = n3;
+        list.push(n3);
+        sum += n3;
+    }
+    console.log(list);
+    return (sum);
 }
-console.log(tong);
+sumFibo(2);
--- bai_02/bai_tap/src/index.ts
+++ bai_02/bai_tap/src/index.ts
@@ -12,21 +12,24 @@
 //     tong += total;
 // }
 // console.log(tong);
+
 function sumFibo(num: number) {
     let n1: number;
     n1 = 0
     let n2: number;
     n2 = 1
-    let total: number;
-    let tong: number;
-    tong = 1
+    let n3: number;
+    let sum: number;
+    sum = 1
+    let list:Array<number>=[];
     for (let i = 1; i <= num; i++) {
-        total = n1 + n2;
+        n3 = n1 + n2;
         n1 = n2;
-        n2 = total;
-        tong += total;
+        n2 = n3;
+        list.push(n3)
+        sum += n3;
     }
-    console.log(tong);
+    console.log(list);
+    console.log(sum);
 }
-
-sumFibo(9);
+sumFibo(2);
